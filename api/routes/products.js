058345f0c69f33d25ed1da01aa4c8b4101f060fa
Mert Şimşek <merttalhasimsek@gmail.com>
2024-03-13 15:42:21 +0300

    Update products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -64,9 +64,18 @@ router.patch('/:productId', (req, res, next) => {
     });
 });
 
-router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product'
-    });
+router.delete("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    Product.deleteOne({_id: id})
+        .exec()
+        .then(result =>{
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
 });
 module.exports = router;
